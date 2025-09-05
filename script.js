6182e8f62a3254a8fd2718457ab7b6ef6d5fb1b4
Sudip Koirala <soodeep321@gmail.com>
2025-09-05 21:09:46 +0545

    updated

--- script.js
+++ script.js
@@ -1,40 +1,70 @@
 console.log("lets e+write js")
+let currSong = new Audio();
 
-// async function main() {
-//     let a = await fetch("http://127.0.0.1:5500/songs/")
-//     let response = await a.text();
-//     console.log(response)
-// }
 
-const getSongs = async ()=>{
+const getSongs = async () => {
     let a = await fetch("http://127.0.0.1:5500/songs/")
     let response = await a.text();
     console.log(response)
     let div = document.createElement("div")
     div.innerHTML = response;
     let as = div.getElementsByTagName("a")
-    let songs= [];
+    let songs = [];
+    let geet = [];
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith(".mp3")){
-songs.push(element.href)
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1].split(".mp3")[0])
+            geet.push(element.href)
         }
-        
+
     }
-    return songs
+    return { songs, geet };
+}
+const playMusic = (track)=>{
+    currSong.src = track;
+    currSong.play();
 }
 
+const main = async () => {
 
-const main = async()=>{
-    let songs = await getSongs()
-    console.log(songs)
+    let { songs, geet } = await getSongs()
+    // console.log(songs)
+    console.log(geet);
+    let songul = document.querySelector(".leftlib ul")
+    for (const song of songs) {
+        songul.innerHTML = songul.innerHTML + ` 
+        <li class="flex">
+                        <img src="music.svg" alt="music">
+                <div class="songinfo">
+                    <div class="songname">${song.replaceAll("%20", " ").replace("%2C", "").replace(/\(.*?\)/g, "")}</div>
+                    <div class="songartist">Sudip</div>
+                </div>
+                <div class="playnow flex">
+                        <span>Play Now</span>
+                        <img src="play.svg" id="lplay" alt="play">
+                    </div>
+                    </li>`;
 
-   const btn = document.getElementById("play"); // a button in your HTML
-    btn.addEventListener("click", () => {
-        var audio = new Audio(songs[0]);
-        audio.play().catch(err => console.log("Playback blocked:", err));
-    });
-   
+    }
+
+    document.querySelectorAll(".songlist li").forEach((li, index) => {
+    li.addEventListener("click", () => {
+        playMusic(geet[index]);
+        })
+        
+        
+    })
+ const play = document.getElementById("play");
+play.addEventListener("click",()=>{
+if (currSong.paused){
+    currSong.play()
+    play.src = "pause.svg"
+}else{
+    currSong.pause()
+play.src = "play.svg"
+}
+})
 }
 
 main()
